33258726670d8d41840f3af57a506edb30adebf1
Aadarsh Singh <aadarshkumarsingh198@gmail.com>
2025-03-14 23:43:54 +0530

    Fix webpack issue?

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
+    
     if (!isServer) {
       config.resolve.fallback = {
           fs: false
       }
+    return config
   }
   },
 };
